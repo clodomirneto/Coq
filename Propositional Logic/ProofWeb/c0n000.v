68df00a79511e0b05baaa246616acc0c1ca63efc
Clodomir Neto <clodomirneto@gmail.com>
2020-06-26 00:54:41 -0300

    Lógica Modal a ser finalizado

--- Propositional Logic/ProofWeb/c0n000.v
+++
@@ -1,9 +0,0 @@
-Require Import ProofWeb.
-
-Parameter A : Prop.
-
-Theorem c0n000 : (A -> A).
-Proof.
-imp_i H.
-exact H.
-Qed.
--- Propositional Logic/ProofWeb/c0n044.v
+++
@@ -1,9 +0,0 @@
-Require Import ProofWeb.
-
-Parameter A : Prop.
-
-Theorem c0n044 : (A \/  True).
-Proof.
-dis_i2.
-tru_i.
-Qed.
