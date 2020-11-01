123a755e26b06145697d3d2419c8a4dc87b5e767
Clodomir Neto <clodomirneto@gmail.com>
2020-11-01 00:24:56 -0300

    Tarefa Estudos Orientados 1

--- Propositional Logic/abs.v
+++
@@ -1,9 +0,0 @@
-Variables A B : Prop.
-Hypothesis P1 : A -> B.
-Theorem abs : A -> (A /\ B).
-Proof.
-intro Ha.
-split.
-- exact Ha.
-- apply P1. exact Ha.
-Qed.
--- Propositional Logic/inc.v
+++
@@ -1,7 +0,0 @@
-Variables A B : Prop.
-Hypothesis P1 : A.
-Hypothesis P2 : ~A.
-Theorem inc : B.
-Proof.
-contradiction.
-Qed.
